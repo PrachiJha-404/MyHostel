1c67e30fdb49569718eef2345dd87d6239848536
PrachiJha-404 <prachijha1805@gmail.com>
2024-11-07 12:26:56 +0530

    Add React app and necessary files

---
+++ myhostel/src/hostel-app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Laundry from './laundry';
+
+function App() {
+    return (
+        <div>
+            <Laundry />
+        </div>
+    );
+}
+
+export default App;
